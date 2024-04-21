4700217b0b69905c870d266ede3ef694335a4587
Carlos Rodríguez - Pardo <crp1994@gmail.com>
2024-04-21 12:29:27 +0200

    Removed build

--- build/lib/textile/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from textile.textile import Textile
-from textile.utils.image_utils import read_and_process_image
-from textile.utils.misc import MyProgressBar
-from textile.utils.create_model import CreateModel
